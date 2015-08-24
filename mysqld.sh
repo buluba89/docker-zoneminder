fe9b3501a7b7c016727b1711be4d1715c32f3401
John BUluba <buluba89@gmail.com>
2015-08-24 21:07:03 +0300

    recreate database if it doenst exist

--- mysqld.sh
+++ mysqld.sh
@@ -21,4 +21,28 @@ else
         if [ -n "$ZM_DB_PASS" ]; then
                 sed -i 's,^\(ZM_DB_PASS=\).*,\1'$ZM_DB_PASS',' /etc/zm/zm.conf
         fi
+        
+        #All tables that zoneminder has 
+        ZM_TABLES=(Config ControlPresets Controls Devices Events Frames Groups Logs MonitorPresets Monitors States Stats TriggersX10 Users ZonePresets Zones)
+        
+        #Check if all tables exists in the database  
+        for i in "${ZM_TABLES[@]}"; do
+
+               #If one doesnt exists recreate database
+               if [ $(mysql -N -s -u $ZM_DB_USER -p$ZM_DB_PASS --host=$ZM_DB_HOST  -e \
+                                               "select count(*) from information_schema.tables where \
+                                                table_schema='${ZM_DB_NAME}' and table_name='${i}';") -ne 1 ]; then
+                      
+                      echo table $i does not exists, recreating database
+
+                      mysql -u $ZM_DB_USER -p$ZM_DB_PASS --host=$ZM_DB_HOST $ZM_DB_NAME < /usr/share/zoneminder/db/zm_create.sql
+
+                      echo Database recreated
+                      break
+
+               fi
+        done
+	
+	#Sleep forever so the init doesnt try to restart this service
+	exec /bin/sleep infinity
 fi
